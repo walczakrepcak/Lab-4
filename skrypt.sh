2eec8faacb92d32b9be385a434146fe1a0d5cd7e
walczakrepcak <kacperwalczakjarocin@gmail.com>
2025-06-22 20:33:16 +0200

    Dodano funkcjonalnosc --logs (100 plikow)

--- skrypt.sh
+++ skrypt.sh
@@ -2,4 +2,13 @@
 
 if [ "$1" == "--date" ]; then
     echo "Dzisiejsza data: $(date +%Y-%m-%d)"
+elif [ "$1" == "--logs" ]; then
+    for i in $(seq 1 100); do
+        FILENAME="log${i}.txt"
+        echo "Nazwa pliku: $FILENAME" > "$FILENAME"
+        echo "Nazwa skryptu: skrypt.sh" >> "$FILENAME"
+        echo "Data utworzenia: $(date +%Y-%m-%d_%H-%M-%S)" >> "$FILENAME"
+        echo "Utworzono $FILENAME"
+    done
+    echo "Utworzono 100 plikow logx.txt."
 fi
