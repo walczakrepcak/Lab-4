eec152b9cd0e556e122353e53f6bd2e995b229ae
walczakrepcak <kacperwalczakjarocin@gmail.com>
2025-06-22 20:55:21 +0200

    Dodano komunikat pomocy dla --help

--- skrypt.sh
+++ skrypt.sh
@@ -1,26 +1,24 @@
 #!/bin/bash
 
-# Uzycie konstrukcji 'case' dla elastycznego parsowania argumentow
-case "$1" in
-    "--date")
-        echo "Dzisiejsza data: $(date +%Y-%m-%d)"
-        ;;
-    "--logs")
-        # Okreslenie liczby plikow logow do utworzenia. Domyślnie 100, jesli drugi argument jest pusty.
-        NUM_FILES=${2:-100}
-        for i in $(seq 1 "$NUM_FILES"); do
-            FILENAME="log${i}.txt"
-            # Zapis nazwy pliku, nazwy skryptu i daty do pliku
-            echo "Nazwa pliku: $FILENAME" > "$FILENAME"
-            echo "Nazwa skryptu: skrypt.sh" >> "$FILENAME"
-            echo "Data utworzenia: $(date +%Y-%m-%d_%H-%M-%S)" >> "$FILENAME"
-        done
-        echo "Utworzono ${NUM_FILES} plikow logx.txt."
-        ;;
-    # Tutaj beda dodawane kolejne warunki dla flag, np. --help, --init, --error
-    # ...
-    *)
-        # Domyślny komunikat dla nieznanych opcji
-        echo "Nieznana opcja. Uzyj --help aby zobaczyc dostepne opcje."
-        ;;
-esac
+if [ "$1" == "--date" ]; then
+    echo "Dzisiejsza data: $(date +%Y-%m-%d)"
+elif [ "$1" == "--logs" ]; then
+    NUM_FILES=${2:-100}
+    for i in $(seq 1 "$NUM_FILES"); do
+        FILENAME="log${i}.txt"
+        echo "Nazwa pliku: $FILENAME" > "$FILENAME"
+        echo "Nazwa skryptu: skrypt.sh" >> "$FILENAME"
+        echo "Data utworzenia: $(date +%Y-%m-%d_%H-%M-%S)" >> "$FILENAME"
+    done
+    echo "Utworzono ${NUM_FILES} plikow logx.txt."
+elif [ "$1" == "--help" ]; then
+    echo "Uzycie: skrypt.sh [OPCJA] [ARGUMENT]"
+    echo ""
+    echo "Opcje:"
+    echo "  --date         : Wyswietla dzisiejsza date w formacie RRRR-MM-DD."
+    echo "  --logs [LICZBA]: Tworzy pliki logx.txt. Domyślnie 100 plikow."
+    echo "                   Kazdy plik zawiera swoja nazwe, nazwe skryptu i date."
+    echo "  --help         : Wyswietla ten komunikat pomocy."
+else
+    echo "Nieznana opcja. Uzyj --help aby zobaczyc dostepne opcje."
+fi
