6f1d68fbd6ba6149921ddfc816bfb3ead41af3ca
walczakrepcak <kacperwalczakjarocin@gmail.com>
2025-06-22 21:03:59 +0200

    Dodano krotkie flagi (-d, -l, -h)

--- skrypt.sh
+++ skrypt.sh
@@ -1,24 +1,29 @@
 #!/bin/bash
 
-if [ "$1" == "--date" ]; then
-    echo "Dzisiejsza data: $(date +%Y-%m-%d)"
-elif [ "$1" == "--logs" ]; then
-    NUM_FILES=${2:-100}
-    for i in $(seq 1 "$NUM_FILES"); do
-        FILENAME="log${i}.txt"
-        echo "Nazwa pliku: $FILENAME" > "$FILENAME"
-        echo "Nazwa skryptu: skrypt.sh" >> "$FILENAME"
-        echo "Data utworzenia: $(date +%Y-%m-%d_%H-%M-%S)" >> "$FILENAME"
-    done
-    echo "Utworzono ${NUM_FILES} plikow logx.txt."
-elif [ "$1" == "--help" ]; then
-    echo "Uzycie: skrypt.sh [OPCJA] [ARGUMENT]"
-    echo ""
-    echo "Opcje:"
-    echo "  --date         : Wyswietla dzisiejsza date w formacie RRRR-MM-DD."
-    echo "  --logs [LICZBA]: Tworzy pliki logx.txt. Domyślnie 100 plikow."
-    echo "                   Kazdy plik zawiera swoja nazwe, nazwe skryptu i date."
-    echo "  --help         : Wyswietla ten komunikat pomocy."
-else
-    echo "Nieznana opcja. Uzyj --help aby zobaczyc dostepne opcje."
-fi
+case "$1" in
+    "--date" | "-d")
+        echo "Dzisiejsza data: $(date +%Y-%m-%d)"
+        ;;
+    "--logs" | "-l")
+        NUM_FILES=${2:-100}
+        for i in $(seq 1 "$NUM_FILES"); do
+            FILENAME="log${i}.txt"
+            echo "Nazwa pliku: $FILENAME" > "$FILENAME"
+            echo "Nazwa skryptu: skrypt.sh" >> "$FILENAME"
+            echo "Data utworzenia: $(date +%Y-%m-%d_%H-%M-%S)" >> "$FILENAME"
+        done
+        echo "Utworzono ${NUM_FILES} plikow logx.txt."
+        ;;
+    "--help" | "-h")
+        echo "Uzycie: skrypt.sh [OPCJA] [ARGUMENT]"
+        echo ""
+        echo "Opcje:"
+        echo "  --date (-d)          : Wyswietla dzisiejsza date w formacie RRRR-MM-DD."
+        echo "  --logs (-l) [LICZBA]: Tworzy pliki logx.txt. Domyślnie 100 plikow."
+        echo "                         Kazdy plik zawiera swoja nazwe, nazwe skryptu i date."
+        echo "  --help (-h)          : Wyswietla ten komunikat pomocy."
+        ;;
+    *)
+        echo "Nieznana opcja. Uzyj --help lub -h aby zobaczyc dostepne opcje."
+        ;;
+esac
